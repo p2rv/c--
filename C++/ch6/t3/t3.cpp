2d2f328a89bb92e49b545d8cc8f0fa56c83d5593
p2rv <p2rv@hotmail.com>
2014-04-24 02:21:47 +1000

    add to ch6/t3 token value

--- C++/ch6/t3/t3.cpp
+++ C++/ch6/t3/t3.cpp
@@ -2,20 +2,41 @@
 #include <ctype.h>
 #include <string>
 
+enum Token_value
+{
+	NAME,NUMB,END,OPER
+};
+Token_value get_token_type(char const ch);
 
 int main()
 {
 
 	char ch;
+	int num;
 	std::string str;
 	std::cin.get(ch);
-	if(isalpha(ch))
+	switch(get_token_type(ch))
 	{
-		std::cin.putback(ch);
-		std::cin>>str;
+		case NAME:
+			std::cin.putback(ch);
+			std::cin>>str;
+		break;
+		case NUMB:
+			std::cin.putback(ch);
+			std::cin>>num;
+		break;
 	}
-
-	std::cout<<str<<'\n';
+	
+	std::cout<<str<<'\n'<<num<<'\n';
 
 	return 0;
+}
+Token_value get_token_type(char const ch)
+{
+	if(isalpha(ch))
+		return NAME;
+	if(isdigit(ch))
+		return NUMB;
+	else
+		return END;
 }
