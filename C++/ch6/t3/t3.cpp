55eb0c0b58c8ccc9c0e119f281d6d4739a21f636
p2rv <p2rv@hotmail.com>
2014-05-03 15:02:30 +1000

    add splits input string into tokens

--- C++/ch6/t3/t3.cpp
+++ C++/ch6/t3/t3.cpp
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <string>
 #include <map>
+#include <vector>
 
 enum Token_value
 {
@@ -10,13 +11,10 @@ enum Token_value
 enum Token_type
 {
 	NAM,NUM,OPR
-}
+};
 
 Token_value get_token_type(char const ch);
 void get_token();
-bool wait_numb(char const ch,int &numb);
-bool wait_name(char const ch,int &name);
-bool wait_oper(char const ch,int &oper);
 
 int main()
 {
@@ -24,6 +22,74 @@ int main()
 	return 0;
 }
 
+
+void get_token()
+{
+	char ch;
+	std::vector<char> opr;
+	std::vector<int> num;
+	std::vector<std::string> nam;
+	std::vector<Token_type> order;
+	std::map<std::string,int> values;
+	std::string tmp_nam;
+	int tmp_num;
+	char tmp_opr;
+	Token_type tmp_type;
+
+
+    while(1)
+    {
+    	std::cin.get(ch);
+		switch(get_token_type(ch))
+		{
+			case NAME:
+				std::cin.putback(ch);
+				std::cin>>tmp_nam;
+				nam.push_back(tmp_nam);
+				order.push_back(NAM);
+				values[tmp_nam]=0;
+			break;
+			case NUMB:
+				std::cin.putback(ch);
+				std::cin>>tmp_num;
+				num.push_back(tmp_num);
+				order.push_back(NUM);
+			break;
+			case PLUS:case MINUS:case DIV:case MUL:case ASSIGN:
+				opr.push_back(ch);
+				order.push_back(OPR);
+				std::cout<<ch<<'\n';
+				break;
+			case END:
+				int a,b,c;
+				a=b=c=0;
+				for(int i=0;i<order.size();++i)
+				{
+					switch(order[i])
+					{
+						case NAM:
+							std::cout<<nam[a]<<' ';
+							++a;
+							break;
+						case NUM:
+							std::cout<<num[b]<<' ';
+							++b;
+							break;
+						case OPR:
+							std::cout<<opr[c]<<' ';
+							++c;
+							break;
+					}
+				}
+				num.clear();
+				nam.clear();
+				opr.clear();
+				order.clear();
+				std::cout<<'\n';
+			break;
+		}
+    }
+}
 Token_value get_token_type(char const ch)
 {
 	if(isalpha(ch))
@@ -49,71 +115,4 @@ Token_value get_token_type(char const ch)
 		default:
 			return SPACE;	
 	}
-}
-
-void get_token()
-{
-	char ch='';
-	int num=-1; //-1 uses for test result
-	double res=0;
-	std::string str="";
-	Token_value prev,cur=SPACE;
-	std::map<std::string,int> values;
-
-    while(str!="end")
-    {
-		std::cin.get(ch);
-		cur=get_token_type(ch);
-		switch(cur)
-		{
-			case NAME:
-				std::cin.putback(ch);
-				std::cin>>str;
-				std::cout<<str<<'\n';
-			break;
-			case NUMB:
-				std::cin.putback(ch);
-				std::cin>>num;
-				std::cout<<num<<'\n';
-			break;
-			case END:
-				if(opr==ASSIGN || opr==SPACE && str!="")
-					values[str]=num;
-				if(opt==ASSIGN || opr==SPACE && str=="")
-					std::cout<<'\t'<<num;
-
-				break;
-			default:
-				opr=get_token_type(ch);
-				std::cout<<(int)ch<<'\n';\
-			break;
-		}
-    }
-}
-bool wait_numb(char const ch,int &numb)
-{
-	if(get_token_type(ch)!=NUMB)
-		return false;
-	std::cin.putback(ch);
-	std::cin>>numb;
-	return true;
-}
-bool wait_name(char const ch,int &name)
-{
-	if(get_token_type(ch)!=NAME)
-		return false;
-	std::cin.putback(ch);
-	std::cin>>name;
-	return true;	
-}
-bool wait_oper(char const ch,int &oper)
-{
-	Token_value cur=get_token(ch);
-	if(cur==MUL||cur==DIV||cur==PLUS||cur==MINUS)
-	{
-		oper=cur;
-		return true;
-	}
-	else
-		return false;
 }
